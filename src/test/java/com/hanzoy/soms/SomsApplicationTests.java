f40c7be39020ca2f8f345e8e22038b2fd4fd5998
大小姐 <60333385+Hanzoy@users.noreply.github.com>
2021-01-11 02:14:17 +0800

    init

---
+++ src/test/java/com/hanzoy/soms/SomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanzoy.soms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
